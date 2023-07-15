df956aeb4963d65344b9b61b1dc9e42871ddad88
Rafael Marins <rcarinha@hotmail.com>
2023-07-15 12:02:01 -0600

    Refactoring

--- app.py
+++ app.py
@@ -54,8 +54,6 @@ def main():
 @cl.on_chat_start
 async def main():
     res = await cl.AskUserMessage(content="Hello, I am an AI insurance bot that will help you save money on your auto insurance. Let's start. What is your zip code?", timeout=10).send()
-    if res:
-        await cl.langchain_postprocess
 
 
 @cl.langchain_postprocess
