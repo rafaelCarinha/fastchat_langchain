865ab2a01e999008afbe3a0d17adf25058e3d3ce
Rafael Marins <rcarinha@hotmail.com>
2023-07-16 09:49:10 -0600

    Zip File Prompt improvement

--- app.py
+++ app.py
@@ -33,10 +33,8 @@ third_question_answer = ''
 
 prompt_template = """
  You are an AI insurance bot that will help users save money on your auto insurance.
- If the user provides a non-logical answer,
+ If the user provides a zip code that does not have 5 digits,
  respond with the exact message: - "Your answer is not valid".
- If the user provides a zip code that does not have 5 digits, or a zip code that is not part of the United States zip codes,
- respond with the exact message: - "Your zip code is not valid". 
  Otherwise, continue the conversation.
    {input}?
 """
