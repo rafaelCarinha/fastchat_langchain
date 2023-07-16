24230c89a86e7c5a11164728b85399d177904b34
Rafael Marins <rcarinha@hotmail.com>
2023-07-16 16:55:24 -0600

    Code refactored for latest questions

--- app.py
+++ app.py
@@ -6,12 +6,9 @@ import chainlit as cl
 import requests
 
 from langchain import PromptTemplate, LLMChain
-from langchain.tools import Tool
-from langchain.agents import initialize_agent
 
 from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(model="gpt-3.5-turbo")
-from langchain.tools import DuckDuckGoSearchRun
 
 load_dotenv()
 
@@ -19,17 +16,38 @@ is_first_question_asked = False
 is_second_question_asked = False
 is_third_question_asked = False
 
-is_match_response_from_endpoint = False
+questions_dict = {
+    1: "Are you currently insured?",
+    2: "Have you had insurance coverage for at least the last 12 months?",
+    3: "What is your current car insurance company?",
+    4: "What's your zip code?",
+    5: "Is your drivers license currently in good standing?",
+    6: "Have you had 1 or more DUI’s in the last 7 years?",
+    7: "Do you own your home?",
+}
 
-first_question = "What's your zip code?"
-second_question = "What is your current car insurance company?"
-third_question = "What is your name?"
+questions_attribute_dict = {
+    1: "currently_insured",
+    2: "insurance_coverage_12_months",
+    3: "current_car_insurance_company",
+    4: "zip_code",
+    5: "drivers_license_in_good_standing?",
+    6: "had_duis",
+    7: "own_your_home",
+}
+
+questions_answer_dict = {
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+    6: "",
+    7: "",
+}
+
 decline_message = "Thank you for your time! You're not suitable for the insurance quote!"
 success_message = "Thank you for your time. We will email you with a insurance quote!"
-
-first_question_answer = ''
-second_question_answer = ''
-third_question_answer = ''
 
 prompt_template = """
  You are an AI insurance bot that will help users save money on your auto insurance.
@@ -38,15 +56,6 @@ prompt_template = """
  Otherwise, continue the conversation.
    {input}?
 """
-
-search = DuckDuckGoSearchRun()
-search_tool = Tool(name="search_tool", description = "search the net",func = search.run)
-
-tools=[search_tool]
-
-agent = initialize_agent(tools=tools, llm=llm,
-                         agent='zero-shot-react-description',
-                         verbose=True)
 
 
 @cl.langchain_factory(use_async=True)
@@ -59,65 +68,106 @@ def main():
 async def main():
     greeting = f"""
     Hello, I am an AI insurance bot that will help you save money on your auto insurance. 
-    Let's start. {first_question}
+    Let's start. 
+    {questions_dict[1]}
     """
     await cl.Message(content=greeting).send()
 
 
 @cl.langchain_postprocess
 async def postprocess(output: str):
-    global is_first_question_asked
-    global is_second_question_asked
-    global is_third_question_asked
-
-    global first_question_answer
-    global second_question_answer
-    global third_question_answer
 
     print(output)
 
     user_input = output['input']
     ai_response = output['text']
 
-    if not is_first_question_asked:
-        if len(user_input) is not 5:
-            ai_response = 'The provided answer is not a valid US ZIP CODE!'
-        #if not validate_ai_response(ai_response):
-            await cl.Message(content=str(ai_response)).send()
-            await cl.Message(content=str(first_question)).send()
+    if len(questions_answer_dict[1]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 1,
+                                                 {f"{questions_attribute_dict[1]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[2]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 2,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[3]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 3,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{questions_answer_dict[2]}",
+                                                  f"{questions_attribute_dict[3]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[4]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 4,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{questions_answer_dict[2]}",
+                                                  f"{questions_attribute_dict[3]}": f"{questions_answer_dict[3]}",
+                                                  f"{questions_attribute_dict[4]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[5]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 5,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{questions_answer_dict[2]}",
+                                                  f"{questions_attribute_dict[3]}": f"{questions_answer_dict[3]}",
+                                                  f"{questions_attribute_dict[4]}": f"{questions_answer_dict[4]}",
+                                                  f"{questions_attribute_dict[5]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[6]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 6,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{questions_answer_dict[2]}",
+                                                  f"{questions_attribute_dict[3]}": f"{questions_answer_dict[3]}",
+                                                  f"{questions_attribute_dict[4]}": f"{questions_answer_dict[4]}",
+                                                  f"{questions_attribute_dict[5]}": f"{questions_answer_dict[5]}",
+                                                  f"{questions_attribute_dict[6]}": f"{user_input}"},
+                                                 False
+                                                 )
+    elif len(questions_answer_dict[7]) == 0:
+        await validate_ai_response_and_check_api(ai_response,
+                                                 user_input,
+                                                 7,
+                                                 {f"{questions_attribute_dict[1]}": f"{questions_answer_dict[1]}",
+                                                  f"{questions_attribute_dict[2]}": f"{questions_answer_dict[2]}",
+                                                  f"{questions_attribute_dict[3]}": f"{questions_answer_dict[3]}",
+                                                  f"{questions_attribute_dict[4]}": f"{questions_answer_dict[4]}",
+                                                  f"{questions_attribute_dict[5]}": f"{questions_answer_dict[5]}",
+                                                  f"{questions_attribute_dict[6]}": f"{questions_answer_dict[6]}",
+                                                  f"{questions_attribute_dict[7]}": f"{user_input}"},
+                                                 True)
+
+
+async def validate_ai_response_and_check_api(ai_response, user_input, question_index, api_json_body, last_message):
+    global questions_answer_dict
+    if not validate_ai_response(ai_response):
+        await cl.Message(content=str(ai_response)).send()
+        await cl.Message(content=str(questions_dict[question_index])).send()
+    else:
+        questions_answer_dict[question_index] = user_input
+        if not check_fountain_header(api_json_body):
+            reset_global_variabes()
+            await cl.Message(content=decline_message).send()
         else:
-            first_question_answer = user_input
-            if not chech_fountain_header({"zip_code":  f"{user_input}"}):
-                reset_global_variabes()
-                await cl.Message(content=decline_message).send()
-            else:
-                return_message = second_question
-                is_first_question_asked = True
+            if not last_message:
+                return_message = questions_dict[question_index+1]
                 await cl.Message(content=return_message).send()
-    elif not is_second_question_asked:
-        if not validate_ai_response(ai_response):
-            await cl.Message(content=str(ai_response)).send()
-            await cl.Message(content=str(second_question)).send()
-        else:
-            second_question_answer = user_input
-            if not chech_fountain_header({"zip_code":  f"{first_question_answer}", "current_insurance_company":  f"{user_input}"}):
-                reset_global_variabes()
-                await cl.Message(content=decline_message).send()
             else:
-                return_message = third_question
-                is_second_question_asked = True
-                await cl.Message(content=return_message).send()
-    elif not is_third_question_asked:
-        if not validate_ai_response(ai_response):
-            await cl.Message(content=str(ai_response)).send()
-            await cl.Message(content=str(third_question)).send()
-        else:
-            third_question_answer = user_input
-            if not chech_fountain_header({"zip_code":  f"{first_question_answer}", "current_insurance_company":  f"{second_question_answer}", "name":  f"{third_question_answer}"}):
-                reset_global_variabes()
-                await cl.Message(content=decline_message).send()
-            else:
-                is_third_question_asked = True
                 await cl.Message(content=success_message).send()
 
 
@@ -129,15 +179,17 @@ def validate_ai_response(ai_response):
         return True
 
 
-def chech_fountain_header(body):
+def check_fountain_header(body):
     print(body)
 
     response = requests.post("https://chatgpt.fountainheadme.com/api/screener", json=body)
     print(response.text)
-    if response.text == '{"status":"match"}':
-        return True
-    elif response.text == '{"status":"dump"}':
-        return False
+    return True
+    # TODO uncomment the following code when the API is ready and not returning random results
+    # if response.text == '{"status":"match"}':
+    #     return True
+    # elif response.text == '{"status":"dump"}':
+    #     return False
 
 
 def reset_global_variabes():
@@ -148,3 +200,4 @@ def reset_global_variabes():
     is_first_question_asked = False
     is_second_question_asked = False
     is_third_question_asked = False
+
